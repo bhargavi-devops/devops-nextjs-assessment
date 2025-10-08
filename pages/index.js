f579645acdad7bea405539648a4eb4f207fbdaad
Bhargavi <bhargavie333@gmail.com>
2025-10-08 21:52:14 +0530

    index.js

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: 'center', paddingTop: '100px', fontFamily: 'Arial' }}>
+      <h1>🚀 DevOps Internship Assessment</h1>
+      <h2>Next.js | Docker | GitHub Actions | Kubernetes (Minikube)</h2>
+      <p style={{ marginTop: '20px', color: '#555' }}>
+        Successfully containerized and deployed by <strong>[Your Name]</strong>.
+      </p>
+      <p>
+        Image Source: <code>ghcr.io/bhargavi-devops/nextjs-app:latest</code>
+      </p>
+    </main>
+  );
+}
